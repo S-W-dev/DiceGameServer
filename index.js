7de69309f6ca69971ee72df8143b059bb3b352fe
Walter Shewmake <25shewmwalj@washk12.org>
2021-02-16 21:00:30 +0000

    asd

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')({
+  path: '/test',
+  serveClient: false,
+});
+
+io.attach(http, {
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
