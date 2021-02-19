1932615c391fcc2a98ef5495ff6f8f1aee67246d
Walter Shewmake <25shewmwalj@washk12.org>
2021-02-19 05:12:13 +0000

    asd

--- index.js
+++ index.js
@@ -146,8 +146,7 @@ class Room {
 			return this.players.filter(player => { return !player.hasBet }).length == 0
 		}
 
-		room[this.players[0].room].update();
-
+		rooms[this.players[0].room].update();
 	}
 
 	addPlayer(socket, roomIndex) {
