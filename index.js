d10eee7409bd3b170e54ff650274acb598b97b42
Walter Shewmake <25shewmwalj@washk12.org>
2021-02-21 17:40:35 +0000

    dsa

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ class Room {
 	}
 
 	gameLoop() {
-		if (this.players.length >= this.minPlayers && this.players.length <= maxPlayers && this.running) {
+		if (this.players.length >= this.minPlayers && this.players.length <= this.maxPlayers && this.running) {
 			if (this.players.filter(player => { return player.status != PlayerStatus.LOST }).length <= 1) running = false;
 			while (!haveAllPlayersBet()) {
 				this.players.forEach(player => {
